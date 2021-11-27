850bb080903589c95c10519d23fb51323ded28b0
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2021-11-27 20:25:28 +0900

    Update Inference_demo.py

--- Inference_demo.py
+++ Inference_demo.py
@@ -1,6 +1,5 @@
 from mmdet.apis import init_detector, inference_detector, show_result_pyplot
 import mmcv
-In [2]:
 #faster_rcnn
 # config_file = '../configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 # ssd
@@ -11,14 +10,11 @@ config_file = '../configs/faster_rcnn/ssdlite_mobilenetv2_scratch_600e_coco.py"
 # checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 # SSD
 checkpoint_file = '../checkpoints/ssd300_coco_20210803_015428-d231a06e.pth'
-In [3]:
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
-In [4]:
 # test a single image
 img = 'demo.jpg'
 result = inference_detector(model, img)
-In [5]:
 # show the results
 # show_result_pyplot(model, img, result)
 model.show_result(img,result,out_file='img_rst.jpg')
