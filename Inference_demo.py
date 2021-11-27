de956fec13e64ce26e5d140812688593d3f6613a
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2021-11-27 20:21:46 +0900

    Update Inference_demo.py

--- Inference_demo.py
+++ Inference_demo.py
@@ -1,10 +1,16 @@
 from mmdet.apis import init_detector, inference_detector, show_result_pyplot
 import mmcv
 In [2]:
-config_file = '../configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+#faster_rcnn
+# config_file = '../configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+# ssd
+config_file = '../configs/faster_rcnn/ssdlite_mobilenetv2_scratch_600e_coco.py"
 # download the checkpoint from model zoo and put it in `checkpoints/`
 # url: https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
-checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+# faster_rcnn
+# checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+# SSD
+checkpoint_file = '../checkpoints/ssd300_coco_20210803_015428-d231a06e.pth'
 In [3]:
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
