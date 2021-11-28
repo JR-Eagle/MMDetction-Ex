ece6613e016f2e69d71024f055b1f4dabefa805d
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2021-11-28 10:14:34 +0900

    Create RPN_Interfence.py

---
+++ RPN_Interfence.py
@@ -0,0 +1,23 @@
+from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+import mmcv
+
+config_file = '../configs/rpn/rpn_r50_caffe_fpn_1x_coco.py'
+# download the checkpoint from model zoo and put it in `checkpoints/`
+# url: https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
+# checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+# SSD
+checkpoint_file = '../checkpoints/rpn_r50_caffe_fpn_1x_coco_20200531-5b903a37.pth'
+# build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+# test a single image
+img = 'demo.jpg'
+result = inference_detector(model, img)
+# show the results
+# show_result_pyplot(model, img, result)
+model.show_result(img,result,out_file='SSD_Img.jpg')
+# video = mmcv.VideoReader('demo.mp4')
+# id = 0
+# for frame in video:
+# result = inference_detector(model,frame)
+# model.show_result(frame,result,out_file=str(id)+".jpg")
+# id = id + 1
