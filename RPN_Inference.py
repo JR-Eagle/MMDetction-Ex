33fedfa4400f109a3328fa9a8401e2f76bc87ea8
JR-Eagle <46098253+JR-Eagle@users.noreply.github.com>
2021-11-28 11:07:27 +0900

    Update RPN_Inference.py

--- RPN_Inference.py
+++ RPN_Inference.py
@@ -1,12 +1,12 @@
 from mmdet.apis import init_detector, inference_detector, show_result_pyplot
 import mmcv
 
-config_file = '../../configs/rpn/rpn_r50_fpn_1x_coco.py'
+config_file = '../../configs/rpn/rpn_x101_64x4d_fpn_2x_coco.py'
 # download the checkpoint from model zoo and put it in `checkpoints/`
 # url: https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
 # checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 # SSD
-checkpoint_file = '../../checkpoints/rpn_r50_fpn_1x_coco_20200218-5525fa2e.pth'
+checkpoint_file = '../../checkpoints/rpn_x101_64x4d_fpn_2x_coco_20200208-c65f524f.pth'
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 # test a single image
